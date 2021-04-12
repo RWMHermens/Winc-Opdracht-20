9b477e6fc126ac7a16f9fef0617e1d94c85cf316
Rob <78263794+RWMHermens@users.noreply.github.com>
2021-04-12 16:41:04 +0200

    js if/else
    if/else statements

---
+++ Opdracht_020_if_else/script.js
@@ -0,0 +1,23 @@
+const age = 23;
+
+if (age >= 18) {
+    console.log('Je mag naar binnen!');
+} else if (age < 18) {
+    console.log('Je bent nog geen 18 jaar, dus helaas mag je niet naar binnen!');
+}
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log('Mooi, je bent een vrouw!');
+} else {
+    console.log('Helaas, je bent geen vrouw!');
+}
+
+const driverStatus = 'bob';
+
+if (driverStatus == 'bob') {
+    console.log('Goeie reis!');
+} else {
+    console.log('Sleutels inleveren aub!');
+}
